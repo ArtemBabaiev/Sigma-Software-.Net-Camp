6bdae71b31b14355635b179eacca0ed3f00ee523
artem <artdcxx4@gmail.com>
2022-05-19 12:36:53 +0300

    Add project files.

---
+++ Sigma_Software/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_1_and_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixFill.PerformFill();
+        }
+    }
+}
