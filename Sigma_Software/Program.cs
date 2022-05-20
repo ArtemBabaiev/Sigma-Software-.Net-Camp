0d38a57c517579792465f580b15c3ab12eebf626
Artem Babaiev <artdcxx4@gmail.com>
2022-05-20 12:28:07 +0300

    Demonstration of matrix fill and storage fill

--- Sigma_Software/Program.cs
+++ Sigma_Software/Program.cs
@@ -6,7 +6,29 @@ namespace HW_1_and_2
     {
         static void Main(string[] args)
         {
-            MatrixFill.PerformFill();
+            Matrix mtr = new Matrix(5, 5);
+
+            try
+            {
+                mtr.DiagonalFill();
+                mtr.PrintMatrix();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("***********************************************************");
+            mtr.SpiralFill();
+            mtr.PrintMatrix();
+            Console.WriteLine("***********************************************************");
+            mtr.ReverseFill();
+            mtr.PrintMatrix();
+            Console.WriteLine("\n***********************************************************\n");
+
+            Storage storage = new Storage();
+            storage.FillStorage();
+
+
         }
     }
 }
