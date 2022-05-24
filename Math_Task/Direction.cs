8064a93124e0bd0d8adeb4f64fa31f9372bbeb15
Artem Babaiev <artdcxx4@gmail.com>
2022-05-24 11:58:27 +0300

    Direction enum for DiagonalFill in Matrix class

---
+++ Math_Task/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Task
+{
+    enum Direction
+    {
+        RIGHT = -1, DOWN = 1
+        
+    }
+}
