f9e8ed8987cabba4bbe2de432a0b95b7cce4277a
Artem Babaiev <artdcxx4@gmail.com>
2022-05-31 21:03:48 +0300

    class for sorting array in file

--- HW_5/VectorFile.cs
+++ HW_5/VectorFile.cs
@@ -20,6 +20,7 @@ namespace HW_5
                 WriteIntoFile(reader, @"data/tempHalf1.txt", lineCount/2);
                 WriteIntoFile(reader, @"data/tempHalf2.txt", lineCount/2);
             }
+            Merge(@"data/tempHalf1.txt", @"data/tempHalf2.txt", @"data/result.txt");
         }
 
         public static int GetNumberOfElements(string path)
@@ -54,10 +55,132 @@ namespace HW_5
             }
         }
 
-        private static void MergeIntoFile(string firstHalfPath, string secondHalfPath)
+        public static void Merge(string pathInputFile1, string pathInputFile2, string pathOutputFile)
         {
-            _ = File.AppendText(@"result.txt");
+            File.WriteAllText(pathOutputFile, string.Empty);
+            int item1 = 0;
+            int item2 = 0;
+            bool endOfFile1 = false;
+            bool endOfFile2 = false;
+            string temp;
 
+            var inputFile1 = File.OpenText(pathInputFile1);
+            var inputFile2 = File.OpenText(pathInputFile2);
+            temp = inputFile1.ReadLine();
+            if (temp == null)
+            {
+                endOfFile1 = true;
+            }
+            else
+            {
+                item1 = Int32.Parse(temp);
+            }
+            temp = inputFile2.ReadLine();
+            if (temp == null)
+            {
+                endOfFile2 = true;
+            }
+            else
+            {
+                item2 = Int32.Parse(temp);
+            }
+
+            while (!endOfFile1 && !endOfFile2)
+            {
+                if (item1 < item2)
+                {
+                    using (StreamWriter sw = File.AppendText(pathOutputFile))
+                    {
+                        sw.WriteLine(item1);
+                    }
+                    temp = inputFile1.ReadLine();
+                    if (temp == null)
+                    {
+                        endOfFile1 = true;
+                    }
+                    else
+                    {
+                        item1 = Int32.Parse(temp);
+                    }
+
+                }
+                else if (item1 == item2)
+                {
+                    using (StreamWriter sw = File.AppendText(pathOutputFile))
+                    {
+                        sw.WriteLine(item1);
+                    }
+                    temp = inputFile1.ReadLine();
+                    if (temp == null)
+                    {
+                        endOfFile1 = true;
+                    }
+                    else
+                    {
+                        item1 = Int32.Parse(temp);
+                    }
+                    temp = inputFile2.ReadLine();
+                    if (temp == null)
+                    {
+                        endOfFile2 = true;
+                    }
+                    else
+                    {
+                        item2 = Int32.Parse(temp);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(pathOutputFile))
+                    {
+                        sw.WriteLine(item2);
+                    }
+                    temp = inputFile2.ReadLine();
+                    if (temp == null)
+                    {
+                        endOfFile2 = true;
+                    }
+                    else
+                    {
+                        item2 = Int32.Parse(temp);
+                    }
+                }
+            }
+            while (!endOfFile1)
+            {
+                using (StreamWriter sw = File.AppendText(pathOutputFile))
+                {
+                    sw.WriteLine(item1);
+                }
+                temp = inputFile1.ReadLine();
+                if (temp == null)
+                {
+                    endOfFile1 = true;
+
+                }
+                else
+                {
+                    item1 = Int32.Parse(temp);
+                }
+            }
+            while (!endOfFile2)
+            {
+                using (StreamWriter sw = File.AppendText(pathOutputFile))
+                {
+                    sw.WriteLine(item2);
+                }
+                temp = inputFile2.ReadLine();
+                if (temp == null)
+                {
+                    endOfFile2 = true;
+                }
+                else
+                {
+                    item2 = Int32.Parse(temp);
+                }
+            }
         }
+
     }
 }
+
