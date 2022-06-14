aa2c0a3e2a2c82be0eab0838f2a84ce5397ae356
Artem Babaiev <artdcxx4@gmail.com>
2022-06-14 17:05:03 +0300

    Completed task2

---
+++ HW_8/Task2/Task2Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8.Task2
+{
+    class Task2Demo
+    {
+        public static void Start()
+        {
+            var web = new WebStatHandler(@"Task2/txtData/stats.txt");
+            Console.WriteLine(web.GetMostPopularDayOfWeek());
+            Console.WriteLine(web.GetSitesVisitByIp("139.18.150.126"));
+            Console.WriteLine(web.GetMostPopularTime());
+            Console.WriteLine(web.GetMostPopularTimeInDay("sunday"));
+        }
+    }
+}
---
+++ HW_8/Task2/WebStatHandler.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8.Task2
+{
+    class WebStatHandler
+    {
+        private string path;
+
+        public WebStatHandler() : this(default)
+        {
+        }
+
+        public WebStatHandler(string path)
+        {
+            this.Path = path;
+        }
+
+        public string Path { get => path; protected set => path = value; }
+
+        public int GetSitesVisitByIp(string ipAdderss)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (line.Contains(ipAdderss))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public string GetMostPopularDayOfWeek()
+        {
+                Dictionary<String, int> stat = GetDictionaryWithFrequency(line => line.Split(" ")[2], path);
+                
+                if (stat.Count == 0)
+                {
+                    return "";
+                }
+                return GetKeyWithHighsetValue(stat);
+        }
+        
+        public string GetMostPopularTime()
+        {
+            Dictionary<string, int> stat = GetDictionaryWithFrequency(line => line.Split(" ")[1].Split(":")[0], path);
+            
+            if (stat.Count == 0)
+            {
+                return null;
+            }
+
+            string max = GetKeyWithHighsetValue(stat);
+            if (max == null)
+            {
+                return null;
+            }
+            return $"{max}:00:00 - {max}:59:59";
+        }
+
+        public string GetMostPopularTimeInDay(string day)
+        {
+            
+            using (StreamReader reader = new StreamReader(path))
+            {
+                using (StreamWriter writer = File.CreateText(@"Task2/txtData/temp.txt"))
+                {
+                    string line = reader.ReadLine();
+                    if (line.Contains(day))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            Dictionary<string, int> stat = GetDictionaryWithFrequency(line => line.Split(" ")[1].Split(":")[0], @"Task2/txtData/temp.txt");
+            File.Delete(@"Task2/txtData/temp.txt");
+            string max = GetKeyWithHighsetValue(stat);
+            if (max == null)
+            {
+                return null;
+            }
+            return $"{max}:00:00 - {max}:59:59";
+        }
+
+        private Dictionary<String, int> GetDictionaryWithFrequency(Func<string, string> keyEvaluation, string pathToFile)
+        {
+            try
+            {
+                Dictionary<String, int> stat = new();
+                using (StreamReader reader = new StreamReader(pathToFile))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string tempKey = keyEvaluation(reader.ReadLine()).ToLower();
+                        if (tempKey.Length == 0)
+                        {
+                            continue;
+                        }
+                        if (!stat.ContainsKey(tempKey))
+                        {
+                            stat.Add(tempKey, 1);
+                            continue;
+                        }
+                        else
+                        {
+                            stat[tempKey]++;
+                        }
+                    }
+                }
+                return stat;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        
+        private string GetKeyWithHighsetValue(Dictionary<string, int> stat)
+        {
+            try
+            {
+                string max = (from item in stat where item.Value == stat.Max(v => v.Value) select item.Key).ToArray()[0];
+                return max;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
