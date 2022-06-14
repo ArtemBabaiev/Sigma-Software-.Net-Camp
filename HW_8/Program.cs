ecb24e5e964a0569e54295e72fb0d83a5c948127
Artem Babaiev <artdcxx4@gmail.com>
2022-06-14 17:08:19 +0300

    Completed HW 8

---
+++ HW_8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1.Task1Demo.Start();
+            Task2.Task2Demo.Start();
+            //Task3.Task3Demo.Start();
+        }
+    }
+}
