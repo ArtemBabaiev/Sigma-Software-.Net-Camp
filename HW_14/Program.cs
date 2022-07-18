50b14a5b9619905b92d07905db548a18e0c81951
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-07-18 20:55:18 +0300

    Completed HW_14 Task 2 and 3

---
+++ HW_14/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HW_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2.Demo.Start();
+            Console.WriteLine("*".PadLeft(10, '*'));
+            Console.WriteLine("*".PadLeft(10, '*'));
+            Task3.Demo.Start();
+        }
+    }
+}
