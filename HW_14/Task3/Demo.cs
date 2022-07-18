6f32998dd15350c20336c0523f60aa785c536a6c
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-07-18 20:54:14 +0300

    Singleton and Abstract Factory

---
+++ HW_14/Task3/Demo.cs
@@ -0,0 +1,31 @@
+﻿using HW_14.Task3.factories;
+using HW_14.Task3.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_14.Task3
+{
+    internal class Demo
+    {
+        public static void Start()
+        {
+            ProductFactory factory0 = new FoodProductFactory();
+            ProductFactory factory1 = new IndustrialProductFactory();
+            _ = factory0.CreateIndustrialProduct(Task3.enums.IndustrialProductEnum.EQUIPMENT);
+            _ = factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY);
+            List<IProduct> products = new List<IProduct>()
+            {
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+                factory1.CreateFoodProduct(Task3.enums.FoodProductEnum.DIARY),
+            };
+            Storage storage = Storage.getInstance(products);
+        }
+    }
+}
